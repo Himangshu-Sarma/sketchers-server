875d5541a0bfe70ee871107b0f01f33ff6e0746a
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2024-02-07 23:42:17 +0530

    ADD: Socket.io events

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({origin: "http://localhost:3000"}))
+app.use(cors({ origin: "http://localhost:3000" }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: "http://localhost:3000" });
 
 io.on("connection", (socket) => {
+
   console.log("Console connected");
+
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  })
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  })
+
+  socket.on("changeConfiguration", (arg) => {
+    socket.broadcast.emit("changeConfiguration", arg);
+  })
 });
 
 httpServer.listen(5000);
