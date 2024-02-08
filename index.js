0993c89b68816a548069c5a23de1b5c22b38143a
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2024-02-09 00:45:10 +0530

    UPD: Production/Development Links

--- index.js
+++ index.js
@@ -2,27 +2,40 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./.env.local",
+});
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+
+const isDev = app.settings.env === "development";
+const url = isDev ? process.env.LocalURL : process.env.URL;
+console.log("isDev", isDev, " url", url);
+
+app.use(cors({ origin: url }));
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: "http://localhost:3000" });
+const io = new Server(httpServer, { cors: url });
 
 io.on("connection", (socket) => {
-
   console.log("Console connected");
 
-  socket.on("beginPath", (arg) => {
-    socket.broadcast.emit("beginPath", arg);
-  })
+  try {
+    socket.on("beginPath", (arg) => {
+      socket.broadcast.emit("beginPath", arg);
+    });
 
-  socket.on("drawLine", (arg) => {
-    socket.broadcast.emit("drawLine", arg);
-  })
+    socket.on("drawLine", (arg) => {
+      socket.broadcast.emit("drawLine", arg);
+    });
 
-  socket.on("changeConfiguration", (arg) => {
-    socket.broadcast.emit("changeConfiguration", arg);
-  })
+    socket.on("changeConfiguration", (arg) => {
+      socket.broadcast.emit("changeConfiguration", arg);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 httpServer.listen(5000);
